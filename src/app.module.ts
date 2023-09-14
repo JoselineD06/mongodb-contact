5a08e00080b8605bcc9a0082a0052d06791b8e9d
mongodb <joseline.duenas2006@gmail.com>
2023-09-14 10:58:02 -0600

    creacion de seguridad

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+import { ConfigModule } from '@nestjs/config';
 import { ContactModule } from './contact/contact.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
       'mongodb+srv://monkeywit:eR9KQwYoKFTSbfc2@cluster0.zlkc2gx.mongodb.net/monkeywitdb?retryWrites=true&w=majority',
     ),
     ContactModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  UseGuards,
+} from '@nestjs/common';
 
 import { ContactService } from './contact.service';
 import { CreateContactDto } from './dtos/create-contact.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('contact')
 export class ContactController {
   constructor(private contactService: ContactService) {}
 
   @Post()
+  @UseGuards(AuthGuard('basic'))
   async create(@Body(new ValidationPipe()) createContact: CreateContactDto) {
     return this.contactService.create(createContact);
   }
